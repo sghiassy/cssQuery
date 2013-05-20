e824525783f2438c3a46affd80736948eec47936
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-05-19 20:07:00 -0700

    Fixed Bug

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 (function() {
-	window.ç = {};
+	window.ç = {}; //register namespace
 	ç.cssRules = [];
 
 	for(var i = 0, iLen = document.styleSheets.length; i < iLen; i++) {
 		var styleSheet = document.styleSheets[i];
 
-		for(var j = 0, jLen = styleSheet.cssRules.length; j < jLen; j++) {
-			var cssRule = styleSheet.cssRules[j];
+		if(styleSheet.cssRules) {
+			for(var j = 0, jLen = styleSheet.cssRules.length; j < jLen; j++) {
+				var cssRule = styleSheet.cssRules[j];
 
-			if(cssRule instanceof CSSStyleRule) {
-				console.log(cssRule.selectorText);
+				if(cssRule instanceof CSSStyleRule) {
+					console.log(cssRule.selectorText);
 
-				ç.cssRules.push(cssRule.cssText);
-				ç[cssRule.selectorText] = cssRule.style;
+					ç.cssRules.push(cssRule.cssText);
+					ç[cssRule.selectorText] = cssRule.style;
+				}
 			}
 		}
 	}
-	});
 })();
