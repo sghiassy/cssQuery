9ea7739af739d7651cfa3616e6b1b28295fdd1df
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-05-24 23:41:20 -0700

    Changed namespace from ç to c

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (function() {
-	window.ç = {}; //register namespace
-	ç.cssRules = [];
+	window. = {}; //register namespace
+	c.cssRules = [];
 
 	for(var i = 0, iLen = document.styleSheets.length; i < iLen; i++) {
 		var styleSheet = document.styleSheets[i];
@@ -10,10 +10,8 @@
 				var cssRule = styleSheet.cssRules[j];
 
 				if(cssRule instanceof CSSStyleRule) {
-					console.log(cssRule.selectorText);
-
-					ç.cssRules.push(cssRule.cssText);
-					ç[cssRule.selectorText] = cssRule.style;
+					c.cssRules.push(cssRule.cssText);
+					c[cssRule.selectorText] = cssRule.style;
 				}
 			}
 		}
