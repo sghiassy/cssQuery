d40c969eed62da75e8f0e74997af28e66dfa333d
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-05-26 00:41:59 -0700

    Updated logic to avoid Cross-Origin Policy Problems

--- main.js
+++ main.js
@@ -4,16 +4,29 @@
 
 	for(var i = 0, iLen = document.styleSheets.length; i < iLen; i++) {
 		var styleSheet = document.styleSheets[i];
+		
+		if(getDomain(styleSheet.href) == getDomain(window.location.href)) { //Necessary to avoid Cross-Origin Problems
+			if(styleSheet.cssRules) {
+				for(var j = 0, jLen = styleSheet.cssRules.length; j < jLen; j++) {
+					var cssRule = styleSheet.cssRules[j];
 
-		if(styleSheet.cssRules) {
-			for(var j = 0, jLen = styleSheet.cssRules.length; j < jLen; j++) {
-				var cssRule = styleSheet.cssRules[j];
-
-				if(cssRule instanceof CSSStyleRule) {
-					c.cssRules.push(cssRule.cssText);
-					c[cssRule.selectorText] = cssRule.style;
+					if(cssRule instanceof CSSStyleRule) {
+						c.cssRules.push(cssRule.cssText);
+						c[cssRule.selectorText] = cssRule.style;
+					}
 				}
 			}
+		}
+	}
+	
+	function getDomain(url) {
+		var fullDomain = (url.match(/:\/\/(.[^/]+)/)[1]).replace('www.','');
+		var domain = fullDomain.split('.');
+
+		if(domain.length == 2) {
+			return domain[0];
+		} else {
+			return domain[1];
 		}
 	}
 })();
